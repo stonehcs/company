54dd4b74e1edfa6de0f34dc335742da1dce39b3e
majie <majie@windows10.microdone.cn>
2017-12-04 16:10:10 +0800

    修改

--- src/main/java/com/lichi/increaselimit/user/controller/UserController.java
+++ src/main/java/com/lichi/increaselimit/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.lichi.increaselimit.user.controller;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.SignatureException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lichi.increaselimit.common.utils.ResultVoUtil;
 import com.lichi.increaselimit.common.vo.ResultVo;
-import com.lichi.increaselimit.user.entity.LoginUser;
-import com.lichi.increaselimit.user.service.LoginUserService;
+import com.lichi.increaselimit.security.UserUtils;
+import com.lichi.increaselimit.user.entity.User;
+import com.lichi.increaselimit.user.service.UserService;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 
 /**
  * 测试controller
@@ -22,26 +29,27 @@ import io.swagger.annotations.ApiOperation;
  * @author majie
  *
  */
-@Api(description = "登陆的客服")
+@Api(description = "用户信息")
 @RestController
-@RequestMapping("/login-user")
+@RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
-	private LoginUserService loginUserService;
-	
+	private UserService userService;
 	/**
-	 * 获取所有的后台登陆用户
-	 * @return
+	 * 获取当前用户信息
+	 * 解析jwt的token
 	 */
-	@GetMapping
-	@ApiOperation("获取所有的后台登陆客服")
-	public ResultVo<LoginUser> getAllLoginUser() {
+	@GetMapping("/me")
+	@ApiImplicitParams({@ApiImplicitParam(name = "Authorization", value = "认证token", required = true, dataType = "string", paramType = "header",defaultValue="bearer ")})
+	public ResultVo<User> getCurrentUser(HttpServletRequest request)
+			throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
+			IllegalArgumentException, UnsupportedEncodingException {
 		
-		List<LoginUser> list = loginUserService.getAll();
+		String username = UserUtils.getUsername();
+		User user = userService.loadUserInfoByUsername(username);
 		
-		Collections.shuffle(list);
-		return ResultVoUtil.success(list.get(0));
+		return ResultVoUtil.success(user);
 	}
 
 }
