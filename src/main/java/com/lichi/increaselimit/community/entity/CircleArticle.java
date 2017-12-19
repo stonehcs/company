db328dace996f1442828fc2de8d3df804eb489e6
majie <majie@windows10.microdone.cn>
2017-12-19 11:15:57 +0800

    添加首页帖子

---
+++ src/main/java/com/lichi/increaselimit/community/entity/CircleArticle.java
@@ -0,0 +1,23 @@
+package com.lichi.increaselimit.community.entity;
+
+/**
+ * 帖子
+ * @author by majie on 2017/11/15.
+ */
+public class CircleArticle extends Article{
+
+	private static final long serialVersionUID = -9219600390068516393L;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
+}
